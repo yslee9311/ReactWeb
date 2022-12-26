c753d5982b83b0f0380e149709e9a358735b3a21
이영수 <yslee9311@gmail.com>
2022-12-26 17:14:13 +0900

    TEST : state 테스트
    1. state 문자열 변환 테스트
    2. state 정수 변환 테스트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    hello: 'hello app js!',
+    count: 0,
+  }
+
+  handleChange = () => {
+    this.setState({
+      hello: 'bye app js!',
+    })
+  }
+
+  countUp = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
   render() {
-    return <div className="App">hello app js!</div>;
+    return (
+      <div className="App">
+        <div>{this.state.hello}</div>
+        <button onClick={this.handleChange}>Click Me</button>
+        <div>{this.state.count}</div>
+        <button onClick={this.countUp}>Click Me</button>
+      </div>
+    )
   }
 }
 
