2bd0aa8e28d00240e588a96ea920b32fe3b701bf
이영수 <yslee9311@gmail.com>
2022-12-31 18:48:40 +0900

    FEAT : form을 사용한 입력 페이지 구현
    1. 이름과 전화번호 state로 관리
    2. 버튼 기능 중 페이지 리로딩 방지
    3. 자식 컴포넌트(PhoneForm)에서 부모 컴포넌트(App.js)의 함수 호출

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AppTutorial from './tutorial/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App message={'Hello Message'}/>
+    {/* <App message={'Hello Message'}/> */}
+    <AppTutorial />
   </React.StrictMode>
 );
