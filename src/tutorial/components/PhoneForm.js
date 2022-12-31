671a95ef26a561b921ccf61de1c954f41a390817
이영수 <yslee9311@gmail.com>
2022-12-31 19:18:59 +0900

    FEAT : 데이터 추가 및 배열 내 key 사용한 리스트 표현
    1. 배열 데이터 추가
    2. key를 사용하여 배열 업데이트 및 리스트 표혀

--- src/tutorial/components/PhoneForm.js
+++ src/tutorial/components/PhoneForm.js
@@ -41,7 +41,6 @@ class PhoneForm extends Component {
                     name='phone'
                 />
                 <button type="submit">등록</button>
-                <div>{this.state.name} {this.state.phone}</div>
             </form>
         );
     }
